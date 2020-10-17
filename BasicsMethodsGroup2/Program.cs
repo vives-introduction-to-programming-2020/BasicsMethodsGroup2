5e5fe2245a3031d65532df7e24b4b3b09e6f8801
Nico De Witte <nico.dewitte@vives.be>
2020-10-17 08:53:32 +0200

    Add code

---
+++ BasicsMethodsGroup2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace BasicsMethodsGroup2
+{
+    class Program
+    {
+        public static string GenerateStudentId(string prefix)
+        {
+            Random generator = new Random();
+            int id = generator.Next(10000000);
+
+            string result = "" + id;        // Convert.ToString(id)
+            result = result.PadLeft(7, '0');
+
+            return prefix + result;
+        }
+
+        public static string GenerateStudentEmail(string firstname,
+            string lastname)
+        {
+            string result = firstname + "." + lastname + "@student.vives.be";
+            result = result.ToLower().Replace(" ", "");
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            // 1) Genereer een s/r nummer voor studenten
+            // s0123456 of r0123456
+            string studentID = GenerateStudentId("s");
+            Console.WriteLine("Student ID = " + studentID);
+
+            // 2) Generate student email
+            Console.WriteLine("Please enter your firstname:");
+            string firstname = Console.ReadLine();
+            Console.WriteLine("Please enter your lastname: ");
+            string lastname = Console.ReadLine();
+
+            string email = GenerateStudentEmail(firstname, lastname);
+            Console.WriteLine(email);
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+# Class Demo - Method Exercises Group 2
+
+Demo code of chapter 7 - Methods. Generating student ID and student Email
+
+## How to get this locally
+
+Go to a local directory on your system and open Powershell in that directory. Now clone this repo using git:
+
+```bash
+git clone git@github.com:vives-introduction-to-programming-2020/BasicsMethodsGroup2.git
+```
